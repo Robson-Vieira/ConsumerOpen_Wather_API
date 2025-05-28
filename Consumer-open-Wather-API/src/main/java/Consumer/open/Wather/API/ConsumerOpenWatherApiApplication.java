d1eab762c6631658671ca6a178a7ef9e6cf79417
Robson Vieira <robsonestudos883@gmail.com>
2025-05-28 10:05:39 -0300

    Inicializando o projeto no github

---
+++ Consumer-open-Wather-API/src/main/java/Consumer/open/Wather/API/ConsumerOpenWatherApiApplication.java
@@ -0,0 +1,13 @@
+package Consumer.open.Wather.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerOpenWatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerOpenWatherApiApplication.class, args);
+	}
+
+}
---
+++ Consumer-open-Wather-API/src/test/java/Consumer/open/Wather/API/ConsumerOpenWatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Consumer.open.Wather.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerOpenWatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
