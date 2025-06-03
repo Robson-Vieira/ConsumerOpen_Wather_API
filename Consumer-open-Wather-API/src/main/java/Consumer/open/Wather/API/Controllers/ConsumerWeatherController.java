dd590273203272559a2f02a938321e8b7701aca4
Robson Vieira <robsonestudos883@gmail.com>
2025-06-02 22:54:57 -0300

    Alterando o nome da classe de controle

--- Consumer-open-Wather-API/src/main/java/Consumer/open/Wather/API/Controllers/ConsumerWeatherController.java
+++ Consumer-open-Wather-API/src/main/java/Consumer/open/Wather/API/Controllers/ConsumerWeatherController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 @RequestMapping("/weatherConsumer")
-public class ConsumerGeocodingController {
+public class ConsumerWeatherController {
 
     @Autowired
     public ConsumerGeocodingService service;
